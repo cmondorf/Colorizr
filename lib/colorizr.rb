25082fe749b556a635e80c1e06a844434cf67861
Christian Mondorf <christian@mondorf.dk>
2016-04-10 11:23:27 +0200

    currently not erring out

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -4,20 +4,35 @@ class String
 #   def colorizr
 #
 #   end
-@@COLORS = {:red => 31, :green => 32, :yellow => 33, :blue => 34, :pink => 35, :light_blue => 94,
+  @@COLORS = {:red => 31, :green => 32, :yellow => 33, :blue => 34, :pink => 35, :light_blue => 94,
    :white => 97, :light_grey => 37, :black => 30 }
 
 
-   def self.create_colors
-     @@COLORS.each do |color, value|
-       define_method("\e[#{value}m #{self}\e[0m")
-
-       puts "\e[#{number}m #{self}\e[0m"
+  def self.create_colors
+    @@COLORS.each do |color, value|
+      define_method("\e[#{value}m #{self}\e[0m") do
+        puts "\e[#{number}m #{self}\e[0m"
+      end
+    end
+  end
 
 
 puts "color hash tests"
 
-puts COLORS[:white]
+  def self.colors
+    colors = []
+    @@COLORS.each do |color, value|
+      colors << colors.to_s
+    end
+    puts colors
+  end
+
+  def self.sample_colors
+    @@colors.each do |color, value|
+      "Example for #{color}".send(color)
+    end
+  end
+end
 
 
 # class Numeric
@@ -28,5 +43,3 @@ puts COLORS[:white]
 #     end
 #   end
 # end
-
-STRING.create_colors
