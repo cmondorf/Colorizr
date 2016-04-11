ba825bdc367d27bfe08c0175bdeed392ad89c21b
Christian Mondorf <christian@mondorf.dk>
2016-04-11 12:55:52 +0200

    update after first review

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,30 +1,30 @@
 
 class String
 
-  @@COLORS = {:red => 31, :green => 32, :yellow => 33, :blue => 34, :pink => 35, :light_blue => 94,
-   :white => 97, :light_grey => 37, :black => 30 }
+  @@Colors = {:red => 31, :green => 32, :yellow => 33, :blue => 34, :pink => 35, :light_blue => 94,
+    :white => 97, :light_grey => 37, :black => 30 }
+
 
   def self.create_colors
-    @@COLORS.each do |color, value|
+    @@Colors.each do |color, value|
       self.send(:define_method, color) do
-sa      end
+        puts "\e[#{value}m #{self}\e[0m"
+      end
     end
   end
 
+
   def self.colors
-    color_value_pairs = []
-    @@COLORS.each do |color, value|
-      color_value_pairs << "#{color} - #{value}"
-    end
-    puts color_value_pairs
+    colors = []
+    colors << @@Colors.keys
+    puts colors
   end
 
   def self.sample_colors
-    @@COLORS.each do |color, value|
+    @@Colors.each do |color, value|
       "Example for #{color}".send(color)
     end
   end
 end
-
 
 String.create_colors
