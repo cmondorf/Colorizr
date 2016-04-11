0a12a444dc852e24bea35d24425d0a74cff5e082
Christian Mondorf <christian@mondorf.dk>
2016-04-11 11:11:15 +0200

    README updated

--- colorizr.gemspec
+++ colorizr.gemspec
@@ -2,8 +2,8 @@ Gem::Specification.new do |s|
   s.name        = 'Colorizr'
   s.version     = '0.1.0'
   s.licenses    = ['MIT']
-  s.summary     = "This gem is a metaprogrammatic implementation of a color-changer for use in comman line applications."
-  s.description = "Much longer explanation of the example!"
+  s.summary     = "Method to add color to strings."
+  s.description = "This gem is a metaprogrammatic implementation of a color-changer for use in comman line applications."
   s.authors     = ["Christian Mondorf"]
   s.email       = 'christian@mondorf.dk'
   s.files       = ["lib/colorizr.rb"]
--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,45 +1,30 @@
 
 class String
-#
-#   def colorizr
-#
-#   end
+
   @@COLORS = {:red => 31, :green => 32, :yellow => 33, :blue => 34, :pink => 35, :light_blue => 94,
    :white => 97, :light_grey => 37, :black => 30 }
 
-
   def self.create_colors
     @@COLORS.each do |color, value|
-      define_method("\e[#{value}m #{self}\e[0m") do
-        puts "\e[#{number}m #{self}\e[0m"
-      end
+      self.send(:define_method, color) do
+sa      end
     end
   end
 
-
-puts "color hash tests"
-
   def self.colors
-    colors = []
+    color_value_pairs = []
     @@COLORS.each do |color, value|
-      colors << colors.to_s
+      color_value_pairs << "#{color} - #{value}"
     end
-    puts colors
+    puts color_value_pairs
   end
 
   def self.sample_colors
-    @@colors.each do |color, value|
+    @@COLORS.each do |color, value|
       "Example for #{color}".send(color)
     end
   end
 end
 
 
-# class Numeric
-#   [["add", "+"], ["subtract", "-"], ["multiply", "*",],
-#    ["divide", "/"]].each do |method, operator|
-#     define_method("#{method}_2") do
-#       method(operator).call(2)
-#     end
-#   end
-# end
+String.create_colors
